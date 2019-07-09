79b44b352f5ae01f3d914e20727a72b183231881
Naoaki Obiki <obiki.wk@gmail.com>
2019-07-09 20:51:11 +0900

    :tada: init.

---
+++ add-package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install -y git \
+  binutils binutils-dev \
+  bison libbison-dev \
+  gawk \
+  build-essential \
+  gcc libgcc-8-dev \
+  g++ \
+  m4 \
+  make cmake \
+  patch \
+  texinfo \
